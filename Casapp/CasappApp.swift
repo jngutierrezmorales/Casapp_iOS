85d1b8d9e4e93563d968911257bde5284e6864b1
Juan Gutierrez <juan.gutierrez@auro-group.com>
2022-11-05 13:37:39 +0100

    Initial

---
+++ Casapp/CasappApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasappApp.swift
+//  Casapp
+//
+//  Created by Juan Gutierrez on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CasappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
